87529cb445f6f7763c97ebdaa8882b2dce852b21
Martin Fosby <marfos19@gmail.com>
2024-03-13 14:33:59 +0100

    updated auth.py

--- flask_app/app/auth.py
+++ flask_app/app/auth.py
@@ -1,4 +1,5 @@
 from flask_app.app.models import User
+from flask_app.app.extensions import db
 from flask_app.app.extensions import login_manager
 
 
@@ -7,5 +8,6 @@ login_manager.login_view = "user.login"
 
 @login_manager.user_loader
 def load_user(user_id):
-    return User.query.get(int(user_id))
+    user = db.session.query(User).get(int(user_id))
+    return user
 
