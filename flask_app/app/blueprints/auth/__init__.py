857d156260dcc19b01095af820e06719946af143
martinfosby <marfos19@gmail.com>
2024-03-15 13:01:08 +0100

    removed url_prefix /auth for auth blueprint

--- flask_app/app/blueprints/auth/__init__.py
+++ flask_app/app/blueprints/auth/__init__.py
@@ -1,5 +1,5 @@
 from flask import Blueprint
 
-bp = Blueprint("auth", __name__, static_folder="static", template_folder="templates", url_prefix="/auth")
+bp = Blueprint("auth", __name__, static_folder="static", template_folder="templates")
 
 from flask_app.app.blueprints.auth import views
