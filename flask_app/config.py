ebc6ba794592e44d2117fe9bf78269bb4826f152
martinfosby <marfos19@gmail.com>
2024-03-19 01:01:23 +0100

    Updated config to have ProductionConfig, DevelopmentConfig and TestingConfig

--- flask_app/config.py
+++ flask_app/config.py
@@ -7,26 +7,46 @@ load_dotenv()
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
-    # Azure server details
-    # SERVER = os.getenv("SERVER") # Your Azure MySQL server name
-    # DATABASE = os.getenv("DATABASE")  # Your MySQL database name
-    # USERNAME = os.getenv("USERNAME")# Your MySQL username
-    # PASSWORD = os.getenv("PASSWORD")  # Your MySQL password
-    SERVER="bv-server-mysql.mysql.database.azure.com"  # Your Azure MySQL server name
-    DATABASE="myDb"  # Your MySQL database name
-    USERNAME="bodovision"  # Your MySQL username
-    PASSWORD="veldig bra Grupp3"  # Your MySQL password
+   
+
+    # Unit testing
+    TESTING = False
 
     # App config
     # NAME = 'Insurance'
     SECRET_KEY = secrets.token_urlsafe(16)
     # sqlalchemy config
-    SQLALCHEMY_DATABASE_URI = f'mysql+mysqlconnector://{USERNAME}:{PASSWORD}@{SERVER}/{DATABASE}'
     SQLALCHEMY_TRACK_MODIFICATIONS = True
     SQLALCHEMY_ECHO = True  # Enable echoing of SQL statements
     # flask-login config
     USE_SESSION_FOR_NEXT = True
 
+    # Logging config
+    LOG_FILE = 'app.log'
+    LOG_LEVEL = 'INFO'
 
 
+class ProductionConfig(Config):
+    """Uses production database server."""
+     # Azure server details
+    # DB_SERVER = os.getenv("DB_SERVER") # Your Azure MySQL server name
+    # DATABASE = os.getenv("DATABASE")  # Your MySQL database name
+    # USERNAME = os.getenv("USERNAME")# Your MySQL username
+    # PASSWORD = os.getenv("PASSWORD")  # Your MySQL password
+    DB_SERVER="bv-server-mysql.mysql.database.azure.com"  # Your Azure MySQL server name
+    DATABASE="myDb"  # Your MySQL database name
+    USERNAME="bodovision"  # Your MySQL username
+    PASSWORD="veldig bra Grupp3"  # Your MySQL password
+    SQLALCHEMY_DATABASE_URI = f'mysql+mysqlconnector://{USERNAME}:{PASSWORD}@{DB_SERVER}/{DATABASE}'
 
+
+class DevelopmentConfig(Config):
+    DB_SERVER = 'localhost'
+    DATABASE_URI = 'sqlite:///:sandbox:'
+    SQLALCHEMY_DATABASE_URI = DATABASE_URI
+
+
+class TestingConfig(Config):
+    DB_SERVER = 'localhost'
+    DATABASE_URI = 'sqlite:///:memory:'
+    SQLALCHEMY_DATABASE_URI = DATABASE_URI
