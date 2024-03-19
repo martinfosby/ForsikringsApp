0aa2d239a765e92d804e6e171c2933bb064255fa
martinfosby <marfos19@gmail.com>
2024-03-19 15:43:54 +0100

    updated login, register, change_username and change_password to have bootstrap plus added remember me in loginform and login page

--- flask_app/app/blueprints/auth/forms/login_form.py
+++ flask_app/app/blueprints/auth/forms/login_form.py
@@ -1,6 +1,8 @@
+from wtforms import BooleanField
 from . import *
 
 class LoginForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
+    remember_me = BooleanField('Remember Me')
     submit = SubmitField('Login')
